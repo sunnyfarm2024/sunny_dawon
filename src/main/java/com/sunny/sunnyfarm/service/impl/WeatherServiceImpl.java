4fd31e5147fabe0a04028a34350b9304c36a9a50
dawon <six29_@naver.com>
2024-12-05 11:01:53 +0900

    20241205

--- src/main/java/com/sunny/sunnyfarm/service/impl/WeatherServiceImpl.java
+++ src/main/java/com/sunny/sunnyfarm/service/impl/WeatherServiceImpl.java
@@ -1,4 +1,19 @@
 package com.sunny.sunnyfarm.service.impl;
 
-public class WeatherServiceImpl {
+import com.sunny.sunnyfarm.service.WeatherServise;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WeatherServiceImpl implements WeatherServise {
+
+    private static final String API_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0";
+    @Value("${weather.api-key}")
+    private String apiKey;
+
+    public void printApiKey() {
+        System.out.println("API Key: " + apiKey);
+    }
 }
